dda3e0cd6d7a37259db2cf913c798f492a175adf
Ben Drucker <bvdrucker@gmail.com>
2015-04-02 22:47:08 -0400

    Rewrite to support use of gulp-style globs

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 'use strict';
 
 import Promise from 'bluebird';
-import babel from 'babel-core';
+import * as babel from 'babel';
 import glob from 'glob';
+import globBase from 'glob2Base';
 import fs from 'fs';
-import {basename} from 'path';
+import {resolve} from 'path';
+import merge from 'deepmerge';
+import writeFile from 'output-file';
 
-glob = Promise.promisify(glob);
+writeFile = Promise.promisify(writeFile);
 Promise.promisifyAll(fs);
+Promise.promisifyAll(babel);
 
-export default function babelify () {
-  return fs.mkdirAsync('./release/es5')
-    .then(function () {
-      return glob('./src/*.js');
-    })
-    .map(function (file) {
-      const filename = basename(file);
-      return fs.readFileAsync(file)
-        .then(function (code) {
-          return babel.transform(code);
-        })
-        .then(function (code) {
-          fs.writeFileAsync(`./release/es5/${filename}`, code);
-        });
+export default function babelify (pack, config = {}) {
+  config = merge({
+    src: './src/**/*.js'
+  }, config);
+  let basePath;
+  return new Promise((resolve, reject) => {
+    const globber = glob(config.src, function (err, results) {
+      if (err) return reject(err);
+      return resolve(results);
     });
+    basePath = globBase(globber);
+  })
+  .map((file) => {
+    return babel.transformFileAsync(file)
+      .get('code')
+      .then((code) => {
+        return writeFile(resolve(config.dest, file.split(basePath)[1]), code);
+      });
+  });
 }
