88f3b39db7c21da7493365855fd9af6e1585beb9
Ben Drucker <bvdrucker@gmail.com>
2015-04-19 19:58:59 -0400

    Adhere to JS standard style

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
-'use strict';
+'use strict'
 
-import Promise from 'bluebird';
-import * as babel from 'babel';
-import globC from 'glob';
-import globBase from 'glob2Base';
-import {Minimatch} from 'minimatch';
-import fs from 'fs';
-import {resolve, join, relative, dirname} from 'path';
-import merge from 'deepmerge';
-import outputFile from 'output-file';
-import strip from 'strip-path';
-import {normalize as normalizeDot} from 'dot-slash';
+import Promise from 'bluebird'
+import * as babel from 'babel'
+import globC from 'glob'
+import globBase from 'glob2Base'
+import {Minimatch} from 'minimatch'
+import fs from 'fs'
+import {resolve, join, relative, dirname} from 'path'
+import merge from 'deepmerge'
+import outputFile from 'output-file'
+import strip from 'strip-path'
+import {normalize as normalizeDot} from 'dot-slash'
 
-const glob = Promise.promisify(globC);
-const writeFile = Promise.promisify(outputFile);
-Promise.promisifyAll(fs);
-Promise.promisifyAll(babel);
+const glob = Promise.promisify(globC)
+const writeFile = Promise.promisify(outputFile)
+Promise.promisifyAll(fs)
+Promise.promisifyAll(babel)
 
 export function defaults (pack, config) {
   config = merge({
     src: './src/**/*.js'
-  }, config);
+  }, config)
 
   config.srcPath = globBase({
     minimatch: new Minimatch(config.src)
-  });
+  })
 
-  return config;
+  return config
 }
 
 export function build (pack, config) {
@@ -35,31 +35,31 @@ export function build (pack, config) {
       return babel.transformFileAsync(file)
         .get('code')
         .then((code) => {
-          return writeFile(resolve(config.dest, file.split(config.srcPath)[1]), code);
-        });
-    });
+          return writeFile(resolve(config.dest, file.split(config.srcPath)[1]), code)
+        })
+    })
 }
 
 export function after (pack, config) {
-  updateMain(pack, config);
-  spliceBabelify(pack, config);
+  updateMain(pack, config)
+  spliceBabelify(pack, config)
 }
 
 function updateMain (pack, config) {
-  const original = pack.get('main');
-  const srcRelative = relative(dirname(pack.path), config.srcPath);
-  const destRelative = relative(dirname(pack.path), config.dest);
-  const outputMainPath = join(destRelative, strip(original, srcRelative));
-  pack.set('main', normalizeDot(outputMainPath, original));
+  const original = pack.get('main')
+  const srcRelative = relative(dirname(pack.path), config.srcPath)
+  const destRelative = relative(dirname(pack.path), config.dest)
+  const outputMainPath = join(destRelative, strip(original, srcRelative))
+  pack.set('main', normalizeDot(outputMainPath, original))
 }
 
 function spliceBabelify (pack, config) {
-  const transforms = pack.get('browserify.transform');
-  if (!(transforms && transforms.length)) return;
-  const b = 'babelify';
+  const transforms = pack.get('browserify.transform')
+  if (!(transforms && transforms.length)) return
+  const b = 'babelify'
   const index = transforms.findIndex((transform) => {
-    if (transform === b) return true;
-    if (Array.isArray(transform) && transform.includes(b)) return true;
-  });
-  if (index > -1) transforms.splice(index, 1);
+    if (transform === b) return true
+    if (Array.isArray(transform) && transform.includes(b)) return true
+  })
+  if (index > -1) transforms.splice(index, 1)
 }
--- test/index.js
+++ test/index.js
@@ -1,26 +1,26 @@
-'use strict';
+'use strict'
 
-import assert from 'assert';
-import * as es5 from '../';
-import {Package as Pack} from 'packhorse';
-import {sync as rmSync} from 'rimraf';
-import {mkdirSync, readFileSync} from 'fs';
-import {resolve} from 'path';
-import {ncp as cp} from 'ncp';
+/*global describe,beforeEach,afterEach,it*/
+
+import assert from 'assert'
+import * as es5 from '../'
+import {Package as Pack} from 'packhorse'
+import {sync as rmSync} from 'rimraf'
+import {mkdirSync, readFileSync} from 'fs'
+import {resolve} from 'path'
+import {ncp as cp} from 'ncp'
 
 describe('publicist-es5', function () {
-
-  const working = resolve(__dirname, 'working');
-
-  this.timeout(5000);
+  const working = resolve(__dirname, 'working')
+  this.timeout(5000)
 
   beforeEach((done) => {
-    mkdirSync(working);
-    cp(resolve(__dirname, 'fixtures'), working, done);
-  });
+    mkdirSync(working)
+    cp(resolve(__dirname, 'fixtures'), working, done)
+  })
   afterEach(() => {
-    rmSync(working);
-  });
+    rmSync(working)
+  })
 
   function build (testCase) {
     return new Pack(resolve(working, `${testCase}/package.json`))
@@ -29,22 +29,18 @@ describe('publicist-es5', function () {
         return es5.build(pack, es5.defaults(pack, {
           dest: resolve(working, `${testCase}/output`),
           src: resolve(working, `${testCase}/src/*.js`)
-        }));
-      });
+        }))
+      })
   }
-
   describe('main', () => {
-
     it('transpiles es6 to es5 with babel', () => {
       return build('normal').then(() => {
-        assert.equal(require('./working/normal/output'), 'foo');
-      });    
-    });
-
-  });
+        assert.equal(require('./working/normal/output'), 'foo')
+      })
+    })
+  })
 
   describe('after', () => {
-
     it('updates the package.json "main"', () => {
       return new Pack(resolve(working, 'normal/package.json'))
         .read()
@@ -52,10 +48,10 @@ describe('publicist-es5', function () {
           es5.after(pack, es5.defaults(pack, {
             dest: resolve(working, 'normal/output'),
             src: resolve(working, 'normal/src/*.js')
-          }));
-          assert.equal(pack.get('main'), './output');
-        });
-    });
+          }))
+          assert.equal(pack.get('main'), './output')
+        })
+    })
 
     it('updates the package.json "main" with non-index', () => {
       return new Pack(resolve(working, 'main-non-index/package.json'))
@@ -64,56 +60,54 @@ describe('publicist-es5', function () {
           es5.after(pack, es5.defaults(pack, {
             dest: resolve(working, 'main-non-index/output'),
             src: resolve(working, 'main-non-index/src/*.js')
-          }));
-          assert.equal(pack.get('main'), './output/code.js');
-        });
-    });
+          }))
+          assert.equal(pack.get('main'), './output/code.js')
+        })
+    })
 
     it('obeys the "main" use of leading "./"', () => {
       return new Pack(resolve(working, 'normal/package.json'))
         .read()
         .then((pack) => {
-          return pack.set('main', 'src').write();
+          return pack.set('main', 'src').write()
         })
         .then((pack) => {
           es5.after(pack, es5.defaults(pack, {
             dest: resolve(working, 'normal/output'),
             src: resolve(working, 'normal/src/*.js')
-          }));
-          assert.equal(pack.get('main'), 'output');
-        });
-    });
+          }))
+          assert.equal(pack.get('main'), 'output')
+        })
+    })
 
     it('removes the babelify transform', () => {
       return new Pack(resolve(working, 'normal/package.json'))
         .read()
         .then((pack) => {
-          return pack.set('browserify.transform', ['babelify']).write();
+          return pack.set('browserify.transform', ['babelify']).write()
         })
         .then((pack) => {
           es5.after(pack, es5.defaults(pack, {
             dest: resolve(working, 'normal/output'),
             src: resolve(working, 'normal/src/*.js')
-          }));
-          assert.equal(pack.get('browserify.transform.length'), 0);
-        });
-    });
+          }))
+          assert.equal(pack.get('browserify.transform.length'), 0)
+        })
+    })
 
     it('removes the babelify transform with options', () => {
       return new Pack(resolve(working, 'normal/package.json'))
         .read()
         .then((pack) => {
-          return pack.set('browserify.transform', [['babelify', {}]]).write();
+          return pack.set('browserify.transform', [['babelify', {}]]).write()
         })
         .then((pack) => {
           es5.after(pack, es5.defaults(pack, {
             dest: resolve(working, 'normal/output'),
             src: resolve(working, 'normal/src/*.js')
-          }));
-          assert.equal(pack.get('browserify.transform.length'), 0);
-        });
-    });
-
-  });  
-
-});
+          }))
+          assert.equal(pack.get('browserify.transform.length'), 0)
+        })
+    })
+  })
+})
